727873cd85eed178b3e581f4b09449600e05bf32
Jon Kid <jkidera14@gmail.com>
2025-11-17 16:04:10 +0300

    Second Commit

---
+++ src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeroSection from './HeroSection';
+
+const Landing = () => {
+    return (
+        <div>< HeroSection /></div>
+    )
+}
+
+export default Landing;
