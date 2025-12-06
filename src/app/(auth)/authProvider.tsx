c87bd629fc07a3a3b755c09e4296db7535b81418
Jon Kid <jkidera14@gmail.com>
2025-12-06 22:50:21 +0300

    Imlemented the AWS Cognito User Authentication Part

--- src/app/(auth)/authProvider.tsx
+++ src/app/(auth)/authProvider.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+"use client";
+import React, { useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
 
-import { Authenticator } from '@aws-amplify/ui-react';
+import {
+    Authenticator,
+    Heading,
+    Radio,
+    RadioGroupField,
+    useAuthenticator,
+    View,
+} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { useRouter, usePathname } from 'next/navigation';
 
 
 Amplify.configure({
@@ -14,15 +23,156 @@ Amplify.configure({
     },
 });
 
-export default function App() {
+const components = {
+    Header() {
+        return (
+            <View className="mt-4 mb-7">
+                <Heading level={3} className="!text-2xl !font-bold">
+                    HA
+                    <span className="text-secondary-500 font-light hover:!text-primary-300">
+                        VEN
+                    </span>
+                </Heading>
+                <p className="text-muted-foreground mt-2">
+                    <span className="font-bold">Welcome!</span> Please sign-in to continue
+                </p>
+            </View>
+        );
+    },
+    SignIn: {
+        Footer() {
+            const { toSignUp } = useAuthenticator();
+            return (
+                <View className="text-center mt-4">
+                    <p className="text-muted-foreground">
+                        Don&apos;t have an account?{" "}
+
+                        <button
+                            onClick={toSignUp}
+                            className="text-primary hover:underline bg-transparent border-none p-0">
+                            Sign up here
+                        </button>
+                    </p>
+                </View>
+            );
+        },
+    },
+    SignUp: {
+        FormFields() {
+            const { validationErrors } = useAuthenticator();
+
+            return (
+                <>
+                    <Authenticator.SignUp.FormFields />
+                    <RadioGroupField
+                        legend="Role"
+                        name="custom:role"
+                        errorMessage={validationErrors?.["custom:role"]}
+                        hasError={!!validationErrors?.["custom:role"]}
+                        isRequired
+                    >
+                        <Radio value="tenant">Tenant</Radio>
+                        <Radio value="manager">Manager</Radio>
+                    </RadioGroupField>
+                </>
+            );
+        },
+        Footer() {
+            const { toSignIn } = useAuthenticator();
+            return (
+                <View className="text-center mt-4">
+                    <p className="text-muted-foreground">
+                        Already have an account?{" "}
+
+                        <button
+                            onClick={toSignIn}
+                            className="text-primary hover:underline bg-transparent border-none p-0">
+                            Sign in
+                        </button>
+                    </p>
+                </View>
+            );
+        },
+    },
+};
+
+const formFields = {
+    signIn: {
+        username: {
+            placeholder: "Enter your email",
+            label: "Email",
+            isRequired: true
+        },
+        password: {
+            placeholder: "Enter your password",
+            label: "Password",
+            isRequired: true
+        }
+    },
+    signUp: {
+        username: {
+            order: 1,
+            placeholder: "Choose a Username",
+            label: "Username",
+            isRequired: true
+        },
+        email: {
+            order: 2,
+            placeholder: "Enter your email address",
+            label: "Email",
+            isRequired: true
+        },
+        password: {
+            order: 3,
+            placeholder: "Create a password",
+            label: "Password",
+            isRequired: true
+        },
+        confirm_password: {
+            order: 4,
+            placeholder: "Confirm your password",
+            label: "Confirm Password",
+            isRequired: true
+        },
+    },
+};
+
+const Auth = ({ children }: { children: React.ReactNode }) => {
+    const { user } = useAuthenticator((context) => [context.user]);
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const isAuthPage = pathname.match(/^\/(signin|signup)$/);
+    const isDashboardPage =
+        pathname.startsWith("/manager") || pathname.startsWith("/tenants");
+
+    //Redirect authenticated users away from auth pages
+    useEffect(() => {
+        if (user && isAuthPage) {
+            router.push("/");
+        }
+    }, [user, isAuthPage, router]);
+
+    //Allowing access to the Public pages without having the need of authentication
+    if (!isAuthPage && !isDashboardPage) {
+        return (
+            <>
+                {children}
+            </>
+        );
+    }
+
     return (
-        <Authenticator>
-            {({ signOut, user }) => (
-                <main>
-                    <h1>Hello {user.username}</h1>
-                    <button onClick={signOut}>Sign out</button>
-                </main>
-            )}
-        </Authenticator>
+        <div className="h-full">
+            <Authenticator
+                initialState={pathname.includes("signup") ? "signUp" : "signIn"}
+                components={components}
+                formFields={formFields}
+            >
+                {() => <>{children}</>}
+            </Authenticator>
+        </div>
     );
 }
+
+export default Auth;
